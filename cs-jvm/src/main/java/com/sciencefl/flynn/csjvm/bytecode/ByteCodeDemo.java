ba6d0218f94eca67f3f81acdc1aaff0d538f1349
fuliang <fuliang@ssc-hn.com>
2024-12-14 17:18:34 +0800

    更新备忘录模式代码

---
+++ cs-jvm/src/main/java/com/sciencefl/flynn/csjvm/bytecode/ByteCodeDemo.java
@@ -0,0 +1,18 @@
+package com.sciencefl.flynn.csjvm.bytecode;
+
+public class ByteCodeDemo {
+    private int m;
+    private int n = 100;
+    public static final int q= 4257;
+    private String s="ssssssss";
+    public String  z = "cccccc";
+
+    public int inc() {
+        return m + 1;
+    }
+
+    public String good(){
+        System.out.println("aaaaaa");
+        return "bbbbbbb";
+    }
+}
