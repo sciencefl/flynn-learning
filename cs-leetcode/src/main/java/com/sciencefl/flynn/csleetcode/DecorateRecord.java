8df83d7b8eeb58b3e0b734d27536d66850568f3a
flynn <flzhang2017@163.com>
2025-03-24 23:06:21 +0800

    双端队列

---
+++ cs-leetcode/src/main/java/com/sciencefl/flynn/csleetcode/DecorateRecord.java
@@ -0,0 +1,25 @@
+package com.sciencefl.flynn.csleetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class DecorateRecord {
+    public List<List<Integer>> decorateRecord(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
